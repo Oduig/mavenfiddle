79f87c0c2cabbc2a46cfb4fc251b4e19601748bd
Guido Josquin <g.josquin@gmail.com>
2017-03-20 11:41:01 +0100

    Fiddle: clean compile test exec:java --projects derived --also-make
    Problem: parameters 'mainClass' for goal org.codehaus.mojo:exec-maven-plugin:1.6.0:java are missing or invalid

---
+++ core/src/main/java/core/Model.java
@@ -0,0 +1,5 @@
+package core;
+
+public class Model {
+  public final String message = "Hello exec maven in subproject!";
+}
---
+++ derived/src/main/java/derived/MyApp.java
@@ -0,0 +1,9 @@
+package derived;
+
+import core.Model;
+
+public class MyApp {
+  public static void main(String[] args) {
+    System.out.println(new Model().message);
+  }
+}
